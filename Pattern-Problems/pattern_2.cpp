45b86d14aa3c56f00591a74026ff37efa1a510b6
Kaushal Kumar <kaushaljee82@gmail.com>
2025-01-20 11:38:26 +0530

    Pattern_2

---
+++ Pattern-Problems/pattern_2.cpp
@@ -0,0 +1,18 @@
+/*
+*
+**
+***
+****
+*****
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(auto i=0; i<5; i++){
+        for(auto j=0; j<=i; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
