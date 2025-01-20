078b82218228296a4b2374def61101ef902a2e21
Kaushal Kumar <kaushaljee82@gmail.com>
2025-01-20 11:50:33 +0530

    Pattern_5

---
+++ Pattern-Problems/pattern_5.cpp
@@ -0,0 +1,20 @@
+/*
+
+*****
+****
+***
+**
+*
+
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(auto i=0; i<5; i++){
+        for(auto j=0; j<5-i; j++){
+            cout << "*";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
