a34b20aafeae96b70a9418ad9b62dbe794e1f215
Kaushal Kumar <kaushaljee82@gmail.com>
2025-02-02 10:39:05 +0530

    nth fibonacci term using multiple recursion call

--- Recursion/fibonacci.cpp
+++ Recursion/fibonacci.cpp
@@ -1,6 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int fibo(int n){ // Print nth term
+    if(n <= 1)
+        return n;
+    return fibo(n-1) + fibo(n-2);
+}
+
+
 void fibonacci(int a, int b, int term){ // Print n terms of fibonacci series
     if(term < 1)
         return;
@@ -9,8 +16,7 @@ void fibonacci(int a, int b, int term){ // Print n terms of fibonacci series
 }
 
 int main(){
-    int term;
-    cin >> term;
+    int term = 20;
     if(term == 1){
         cout << "0 ";
         return 0;
@@ -18,5 +24,7 @@ int main(){
     int  a = 0, b = 1;
     cout << a << " " << b << " ";
     fibonacci(a, b, term-2);
+    cout << endl;
+    cout << "7th term is " << fibo(7) << endl;
     return 0;
 }
