87af637ec099641ec2738791d60bf5bc880a0e84
Kaushal Kumar <133222412+Kaushal-04@users.noreply.github.com>
2025-05-06 19:18:53 +0530

    Time: 0 ms (100.00%) | Memory: 22.1 MB (76.53%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
-    int candy(vector<int>& ratings) {
-        int n = ratings.size();
-        if(n == 1)
-            return 1;
-        vector<int> count(n,1);
-        for(auto i=1; i<n; i++){
-            if(ratings[i] > ratings[i-1] && count[i] <= count[i-1])
-                count[i] = count[i-1]+1;
+  int candy(vector<int>& ratings) {
+    int n = ratings.size();
+    int candies = n;
+    int i = 1;
 
-        }
-        for(auto i=n-2; i>=0; i--){
-            if(ratings[i] > ratings[i+1] && count[i] <= count[i+1])
-                count[i] = count[i+1]+1;
+    while (i<n){
+      if (ratings[i] == ratings[i-1]){
+        i++;
+        continue;
+      }
 
-        }
-        int sum = 0;
-        for(auto i=0; i<n; i++){
-            sum += count[i];
-        }
-        return sum;
+      int peak = 0;
+      while (ratings[i] > ratings[i-1]){
+        peak++;
+        candies += peak;
+        i++;
+        if (i == n) return candies;
+      }
+
+      int valley = 0;
+      while (i<n && ratings[i] < ratings[i-1]){
+        valley++;
+        candies += valley;
+        i++;
+      }
+
+      candies -= min(peak, valley);
     }
+
+    return candies;
+  }
 };
