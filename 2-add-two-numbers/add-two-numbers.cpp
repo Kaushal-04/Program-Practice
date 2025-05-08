7b01186d8c5605c5e1d0e01e9a751be6fb0553f6
Kaushal Kumar <133222412+Kaushal-04@users.noreply.github.com>
2025-05-08 20:08:52 +0530

    Time: 0 ms (100.00%) | Memory: 77.1 MB (43.30%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -11,54 +11,34 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* result = new ListNode();
-        ListNode* curr = result;
-        string s1="";
+        ListNode* l3=new ListNode(0);
+        ListNode* head=l3;  //l3 start with 0
+        int carry=0;
+        while(l1 && l2){
+            int sum=l1->val+l2->val+carry;
+            carry=sum/10;
+            l3->next= new ListNode(sum%10);
+            l1=l1->next;
+            l2=l2->next;
+            l3=l3->next;
+        }
         while(l1){
-            s1 += l1->val + '0';
-            l1 = l1 -> next;
+            int sum=l1->val+carry;
+            carry=sum/10;
+            l3->next= new ListNode(sum%10);
+            l1=l1->next;
+            l3=l3->next;
         }
-        string s2="";
         while(l2){
-            s2 += l2->val + '0';
-            l2 = l2 -> next;
-        }
-
-        if(s1.length() < s2.length()){
-            while(s1.length() != s2.length()){
-                s1 += '0';
-            }
-        }
-        if(s1.length() > s2.length()){
-            while(s1.length() != s2.length()){
-                s2 += '0';
-            }
-        }
-
-
-        bool carry = false;
-        for(int i=0; i<s1.length(); i++){
-            
-            int s;
-            if(carry)
-                s = (s1[i] - '0') + (s2[i] - '0') + 1;
-            else
-                s = (s1[i] - '0') + (s2[i] - '0');
-            ListNode* temp = new ListNode();
-            temp -> val = s % 10;
-            if(s > 9)
-                carry = true;
-            else
-                carry = false;
-            curr->next = temp;
-            curr = curr->next; 
+            int sum=l2->val+carry;
+            carry=sum/10;
+            l3->next= new ListNode(sum%10);
+            l2=l2->next;
+            l3=l3->next;
         }
         if(carry){
-            ListNode* temp = new ListNode();
-            temp->val = 1;
-            curr->next = temp;
-            curr = curr->next; 
+            l3->next=new ListNode(carry);
         }
-        return result -> next;
+        return head->next;
     }
 };
