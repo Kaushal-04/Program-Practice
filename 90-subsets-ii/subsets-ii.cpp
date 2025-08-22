5314dd469fa902a856692713aeeadf5fb309b0cf
Kaushal Kumar <133222412+Kaushal-04@users.noreply.github.com>
2025-08-22 07:06:50 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (66.93%) - LeetSync

--- 90-subsets-ii/subsets-ii.cpp
+++ 90-subsets-ii/subsets-ii.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
 
-    void help(vector<int>& nums, set<vector<int>>& res, int i, vector<int> curr){
-        if(i == nums.size()){
-            sort(curr.begin(), curr.end());
-            res.insert(curr);
-            return;
+    void help(vector<int>& nums, vector<vector<int>>& res, int i, vector<int>& curr){
+        if(i >= nums.size()){
+            res.push_back(curr);
+            return ;
         }
+
         curr.push_back(nums[i]);
         help(nums, res, i+1, curr);
         curr.pop_back();
+        while (i + 1 < nums.size() && nums[i] == nums[i + 1]) i++;
         help(nums, res, i+1, curr);
     }
 
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> res;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> res;
         vector<int> curr;
         help(nums, res, 0, curr);
-
-        vector<vector<int>> ans(res.begin(), res.end());
-        return ans;
+        return res;
     }
 };
