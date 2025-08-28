fdae4c685ca83f80d2b22fdaa296cae3921b6096
Kaushal Kumar <133222412+Kaushal-04@users.noreply.github.com>
2025-08-28 17:21:55 +0530

    Time: 6 ms (7.61%) | Memory: 16.7 MB (88.58%) - LeetSync

---
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int rows = grid.size();
+        int cols = grid[0].size();
+        queue<pair<int, int>> q;
+        int fresh = 0;
+        for (int i = 0; i < rows; ++i) {
+            for (int j = 0; j < cols; ++j) {
+                if (grid[i][j] == 2) {
+                    q.push({i, j});
+                } else if (grid[i][j] == 1) {
+                    ++fresh;
+                }
+            }
+        }
+
+        if (fresh == 0) return 0; 
+        int time = 0;
+        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        // Step 2: BFS to rot adjacent oranges
+        while (!q.empty()) {
+            int size = q.size();
+            bool rottedThisMinute = false;
+
+            for (int i = 0; i < size; ++i) {
+                auto [x, y] = q.front();
+                q.pop();
+
+                for (auto [dx, dy] : directions) {
+                    int nx = x + dx;
+                    int ny = y + dy;
+
+                    if (nx >= 0 && ny >= 0 && nx < rows && ny < cols && grid[nx][ny] == 1) {
+                        grid[nx][ny] = 2;
+                        q.push({nx, ny});
+                        fresh--;
+                        rottedThisMinute = true;
+                    }
+                }
+            }
+
+            if (rottedThisMinute) ++time;
+        }
+
+        return fresh == 0 ? time : -1;
+    }
+};
