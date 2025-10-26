c9257281cda00e7afe837334bf48d1f91eab4b08
Kaushal Kumar <133222412+Kaushal-04@users.noreply.github.com>
2025-10-26 07:32:13 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (55.25%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 2)  return n;
+        vector<int> dp;
+        dp.push_back(1);
+        dp.push_back(1);
+        dp.push_back(2);
+        for(int i=3; i<=n; i++){
+            dp.push_back( dp[i-1] + dp[i-2]);
+        }
+        return dp[n];
+    }
+};
