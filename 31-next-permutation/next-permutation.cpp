752c31c1dcee39885120afdd8aa82de62330c783
Kaushal Kumar <133222412+Kaushal-04@users.noreply.github.com>
2025-10-29 09:33:53 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (32.61%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,23 +1,26 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        // int n = nums.size();
-        // int ind= - 1;
-        // for(int i= n -2; i>=0; i--){
-        //     if(nums[i] < nums[i+1]){
-        //         ind = i;
-        //     break;}
-        // }
-        // if(ind == -1){
-        //     reverse(nums.begin(), nums.end());
-        //     return ;
-        // }
-        // for(int i= n-1; i>ind; i--){
-        //     if(nums[i] > nums[ind]){
-        //         swap(nums[i], nums[ind]);
-        //     break;}
-        // }
-        // sort(nums.begin() + ind + 1, nums.end());
-        next_permutation(nums.begin(), nums.end());
+        int n = nums.size();
+        int mark = -1;
+        for(int i=n-1; i>0; i--){
+            if(nums[i] > nums[i-1]){
+                mark = i-1;
+                break;
+            }
+        }
+        if(mark == -1){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        int diff = INT_MAX, pos = -1;
+        for(int i=n-1; i>mark; i--){
+            if(nums[i] != nums[mark] && (nums[i] - nums[mark]) < diff && (nums[i] - nums[mark]) > 0){
+                diff = nums[i] - nums[mark];
+                pos = i;
+            }
+        }
+        swap(nums[pos], nums[mark]);
+        sort(nums.begin() + mark + 1, nums.end());
     }
 };
