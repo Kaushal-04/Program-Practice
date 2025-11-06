ee6d3a85696f254fb74972ea7c2f57a3ed9b8c79
Kaushal Kumar <133222412+Kaushal-04@users.noreply.github.com>
2025-11-06 19:02:32 +0530

    Time: 3 ms (67.95%) | Memory: 26.8 MB (64.57%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        vector<int> temp;
         int n = grid.size();
-        int sum =(( n*n )* ((n * n)+1))/2;
-        vector<int> res(2);
-        unordered_set<int> st;
-        int repeat = -1;
-        int actualSum = 0;
-        for(int i=0; i<n; i++){
+        int sum = 0;
+        for(int i=0; i<n; i++)
             for(int j=0; j<n; j++){
-                actualSum += grid[i][j];
-                if(st.find(grid[i][j]) != st.end()){
-                    repeat = grid[i][j];
-                }
-                else st.insert(grid[i][j]);
+                temp.push_back(grid[i][j]);
+                sum += grid[i][j];
             }
-        }
-        res[0] = repeat;
-        res[1] = sum + repeat - actualSum;
-        return res;
+
+        sort(temp.begin(), temp.end());
+        int rep = -1, miss = -1;
+        for(int i=0; i<temp.size()-1; i++){
+            if(temp[i] == temp[i+1]){
+                rep = temp[i];
+                break;
+            }
+        }      
+        miss = (((n*n)*((n*n) + 1))/2) - (sum - rep);
+        return {rep, miss}; 
     }
 };
