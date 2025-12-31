41a9f2c71db679075a1be681de5884260512077b
Kaushal Kumar <133222412+Kaushal-04@users.noreply.github.com>
2025-12-31 18:34:55 +0530

    Time: 0 ms (100.00%) | Memory: 18.5 MB (77.63%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,57 +1,42 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-        
-        bool firstRowZero = false;
-        bool firstColZero = false;
-        
-        // Check if first row has any zeros
-        for (int j = 0; j < n; j++) {
-            if (matrix[0][j] == 0) {
-                firstRowZero = true;
+        int n = matrix.size();
+        int m = matrix[0].size();
+        bool rowAffect = false, colAffect = false;
+        for(int i=0; i<m; i++){
+            if(matrix[0][i] == 0){
+                rowAffect = true;
                 break;
             }
         }
-
-        // Check if first column has any zeros
-        for (int i = 0; i < m; i++) {
-            if (matrix[i][0] == 0) {
-                firstColZero = true;
+        for(int i=0; i<n; i++){
+            if(matrix[i][0] == 0){
+                colAffect = true;
                 break;
             }
         }
-
-        // Mark zeros in the first row and column
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][j] == 0) {
-                    matrix[i][0] = 0;
+        for(int i=1; i<n; i++){
+            for(int j = 1; j<m; j++){
+                if(matrix[i][j] == 0){
                     matrix[0][j] = 0;
+                    matrix[i][0] = 0;
                 }
             }
         }
-
-        // Use the marks to set zeros
-        for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+        for(int i=1; i<n; i++){
+            for(int j = 1; j<m; j++){
+                if(matrix[0][j] == 0 || matrix[i][0] == 0)
                     matrix[i][j] = 0;
-                }
             }
         }
-
-        // Zero out first row if needed
-        if (firstRowZero) {
-            for (int j = 0; j < n; j++) {
-                matrix[0][j] = 0;
+        if(rowAffect){
+            for(int i=0; i<m; i++){
+                matrix[0][i] = 0;
             }
         }
-
-        // Zero out first column if needed
-        if (firstColZero) {
-            for (int i = 0; i < m; i++) {
+        if(colAffect){
+            for(int i=0; i<n; i++){
                 matrix[i][0] = 0;
             }
         }
